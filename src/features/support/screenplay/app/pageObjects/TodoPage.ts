7a5d84ad97270df2b6bab5798079c40259f99dd7
LayMui <laymui.toh@taiger.com>
2020-09-26 22:19:27 +0800

    Demo to trigger the CI/CD pipeline

--- src/features/support/screenplay/app/pageObjects/TodoPage.ts
+++ src/features/support/screenplay/app/pageObjects/TodoPage.ts
@@ -2,6 +2,7 @@ import { Target } from '@serenity-js/protractor';
 import { by } from 'protractor';
 
 export class TodoPage {
+    /* I added some new changes */
     static textInput     = Target.the('username input').located(by.id('todo-text'));
     static addButton     = Target.the('password input').located(by.id('addTodo-btn'));
     static todoList = Target.all('todo list').located(by.css('li span'));
